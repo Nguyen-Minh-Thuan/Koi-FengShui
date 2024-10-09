2d901af2221c0477a4ae1c2b1e65f88c52137266
duclacoder <ducpvse183843@fpt.edu.vn>
2024-10-09 21:21:55 +0700

    fix admin page

--- src/Component/AdminNavbar/index.jsx
+++ src/Component/AdminNavbar/index.jsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const AdminNavbar = () => {
-   const [WebListOpen, setWebManagementList] = useState(false);
-   const [StatisticListOpen, setStatisticList] = useState(false);
+  const [WebListOpen, setWebManagementList] = useState(false);
+  const [StatisticListOpen, setStatisticList] = useState(false);
 
   return (
-    <div className='flex w-[300px] h-[630px] border-r-2 '>
+    <div className='flex w-[300px] h-[630px] border-r-2 shadow-lg '>
       <ul className='w-full'>
         <li className='w-full'>
-          <button 
+          <button
             onClick={() => setWebManagementList(!WebListOpen)}
-            className='w-full h-[40px] px-8 py-2 border-y-2 flex justify-between items-center text-white text-xl font-bold'
-            style={{backgroundColor:'#484848'}}
+            className='w-full h-[40px] px-8 flex justify-between items-center text-white text-xl font-bold boeder-b-2'
+            style={{ backgroundColor: '#4C7A7A' }}
           >
             Web Management
             <span className='ml-2'>{WebListOpen ? '▲' : '▼'}</span>
           </button>
-          {WebListOpen && (
-            <ul className='flex flex-col' style={{color:'#484848'}}>
+          <div
+            className={`overflow-hidden transition-all duration-500 ${
+              WebListOpen ? 'max-h-[300px]' : 'max-h-0'
+            }`}
+          >
+            <ul>
               <li className='w-full h-[40px] px-8 py-2 border-t-2'>
                 <Link to='/admin/accountlist'>Account</Link>
               </li>
@@ -32,25 +36,32 @@ const AdminNavbar = () => {
                 <Link to='/admin/adslist'>Advertise</Link>
               </li>
             </ul>
-          )}
+          </div>
         </li>
 
         <li className='w-full'>
-         <button onClick={() => setStatisticList(!StatisticListOpen)} className='w-full h-[40px] px-8 py-2 border-y-2 flex justify-between items-center text-white text-xl font-bold'
-            style={{backgroundColor:'#484848'}}>
-            Statistic 
+          <button
+            onClick={() => setStatisticList(!StatisticListOpen)}
+            className='w-full h-[40px] px-8 py-2 border-y-2 flex justify-between items-center text-white text-xl font-bold'
+            style={{ backgroundColor: '#4C7A7A' }}
+          >
+            Statistic
             <span className='ml-2'>{StatisticListOpen ? '▲' : '▼'}</span>
-         </button>       
-         {StatisticListOpen && (
-            <ul style={{color:'#484848'}}>
-               <li className='w-full h-[40px] px-8 py-2 border-b-2'>
-                  <Link to='/account'>Dashboard</Link>
-               </li>
-               <li className='w-full h-[40px] px-8 py-2 border-b-2'>
-                  <Link to='/account'>....</Link>
-               </li>
+          </button>
+          <div
+            className={`overflow-hidden transition-all duration-500 ${
+              StatisticListOpen ? 'max-h-[120px]' : 'max-h-0'
+            }`}
+          >
+            <ul>
+              <li className='w-full h-[40px] px-8 py-2 border-b-2'>
+                <Link to='/account'>Dashboard</Link>
+              </li>
+              <li className='w-full h-[40px] px-8 py-2 border-b-2'>
+                <Link to='/account'>....</Link>
+              </li>
             </ul>
-         )}   
+          </div>
         </li>
       </ul>
     </div>
